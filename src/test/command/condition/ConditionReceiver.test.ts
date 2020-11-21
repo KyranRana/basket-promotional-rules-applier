2806c69389f88b198198815dfe733c10b5ee4084
Kyran Rana <kyranportfolio@gmail.com>
2020-11-21 00:03:00 +0000

    Added type to store testcase data in condition test

--- src/test/command/condition/ConditionReceiver.test.ts
+++ src/test/command/condition/ConditionReceiver.test.ts
@@ -1,4 +1,12 @@
+import { Condition } from "../../../main/command/condition/Condition"
 import ConditionReceiver from "../../../main/command/condition/ConditionReceiver"
+
+interface TestCaseData {
+  operator: string
+  operand: number
+  onValue: number
+  expected: boolean
+}
 
 test.each`
   operator | operand | onValue | expected
@@ -8,8 +16,9 @@ test.each`
   ${'<'}   | ${55}   | ${90}   | ${false}
   ${'='}   | ${45}   | ${45}   | ${true}
   ${'='}   | ${45}   | ${44}   | ${false}
-`('execute ($onValue $operator $operand = $expected)', ({ operator, operand, onValue, expected }) => {
-  const condition = { operator, operand }
+`('execute ($onValue $operator $operand = $expected)', (testcase: TestCaseData) => {
+  const { operator, operand, onValue, expected } = testcase
+  const condition = { operator, operand } as Condition<number>
     
   const conditionReceiver = new ConditionReceiver()
   expect(conditionReceiver.execute(condition, onValue)).toBe(expected)
